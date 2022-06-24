6385d8a15d3ff817abe2b694f96b571bb57ca391
Michael Kienbsuch <mkienbus@gmail.com>
2022-06-24 10:36:10 -0600

    change to using iteration

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -20,5 +20,8 @@ if (require.main === module) {
 
 module.exports = reverseString;
 
+// without using built in methods:
+// map over string
+
 // Please add your pseudocode to this file
 // And a written explanation of your solution
